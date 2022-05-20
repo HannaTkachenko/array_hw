5dc8e7fcc2852c5469bb794b5ba8eb0419cf7dfa
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-05-20 22:50:39 +0300

    Add 4 variants for splice

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ console.log(languageArray2);
 
 //Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [1, 2, 3].
 const numberArray = [1, 2, 3, 4, 5];
-const slicedBegin = numberArray.slice(0,3);
+const slicedBegin = numberArray.slice(0, 3);
 
 console.log(slicedBegin);
 
@@ -55,4 +55,28 @@ const slicedEnd = numberArray.slice(-2);
 
 console.log(slicedEnd);
 
+// Дан массив [1, 2, 3, 4, 5]. С помощью метода splice преобразуйте массив в [1, 4, 5].
+const numberArraySplice1 = [1, 2, 3, 4, 5];
+const removedFromArraySplice1 = numberArraySplice1.splice(1,2)
 
+console.log(numberArraySplice1);
+
+//Дан массив [1, 2, 3, 4, 5]. С помощью метода splice запишите в новый массив элементы [2, 3, 4].
+const numberArraySplice2 = [1, 2, 3, 4, 5];
+const removedFromArraySplice2 = numberArraySplice2.splice(1,3)
+
+console.log(removedFromArraySplice2);
+
+// Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 2, 3, 'a', 'b', 'c', 4, 5].
+const numberArraySplice3 = [1, 2, 3, 4, 5];
+const addedToArraySplice3 = numberArraySplice3.splice(3,0,'a','b','c')
+
+console.log(numberArraySplice3);
+
+// Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
+const numberArraySplice4 = [1, 2, 3, 4, 5];
+const firstAddToArraySplice4 = numberArraySplice4.splice(1,0,'a','b')
+const secondAddToArraySplice4 = numberArraySplice4.splice(6,0,'c')
+const thirdAddToArraySplice4 = numberArraySplice4.splice(8,0,'e')
+
+console.log(numberArraySplice4);

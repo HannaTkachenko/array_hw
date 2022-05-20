422281d23f44c7f6f140350e40f44b148a5f33dc
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-05-20 20:53:49 +0300

    Add concat

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
+const array1 = [1, 2, 3];
+const array2 = [4,5,6];
+const array3 = array1.concat(array2);
+
+console.log(array3)
